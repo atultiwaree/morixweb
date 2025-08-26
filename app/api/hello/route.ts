09d0dbb4bf5714dc5f72af6236ff45d99793ae60
atultiwaree <atultiwaree@proton.me>
2025-08-27 01:30:51 +0530

    cloudflare

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,12 @@
-export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
+// export async function GET(request: Request) {
+//   return new Response("Hello, Next.js!");
+// }
+
+
+export const runtime = 'edge';
+
+export default function handler(req) {
+  return new Response(JSON.stringify({ message: 'Hello from Edge!' }), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
